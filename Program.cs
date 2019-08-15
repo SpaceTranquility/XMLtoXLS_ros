4e6c2aced6540a7dc8cd603f121f598140d0cf07
SpaceTR <vanez-study@yandex.ru>
2019-08-15 20:57:04 +0300

    12.08.19  #2

--- Program.cs
+++ Program.cs
@@ -156,70 +156,69 @@ namespace XmlToXls
             application.SheetsInNewWorkbook = 1;
             //XlsForm.Visible = true;
 
+            int firstRow = 0;
+
             application.Workbooks.Add();
             Worksheet sheet = (Worksheet)application.ActiveSheet;
 
             //Размеры            
             sheet.Columns[1].ColumnWidth = 4;
-            sheet.Columns[2].ColumnWidth = 4;
-            sheet.Columns[3].ColumnWidth = 30;
+            sheet.Columns[2].ColumnWidth = 9;
+            sheet.Columns[3].ColumnWidth = 35;
             sheet.Columns[4].ColumnWidth = 70;
-            sheet.Columns[5].ColumnWidth = 10;
-            sheet.Columns[6].ColumnWidth = 10;
-            sheet.Columns[7].ColumnWidth = 10;
+            sheet.Columns[5].ColumnWidth = 13;
+            sheet.Columns[6].ColumnWidth = 13;
+            sheet.Columns[7].ColumnWidth = 13;
 
-            sheet.Rows[2].RowHeight = 65;
-            sheet.Rows[3].RowHeight = 20;
-            sheet.Rows[4].RowHeight = 60;
-            sheet.Rows[5].RowHeight = 40;
+            sheet.Rows[firstRow + 1].RowHeight = 65;
+            sheet.Rows[firstRow + 2].RowHeight = 20;
+            sheet.Rows[firstRow + 3].RowHeight = 60;
+            sheet.Rows[firstRow + 4].RowHeight = 40;
 
-            sheet.Cells.Font.Size = 8;
+            sheet.Cells.Font.Size = 10;
             sheet.Cells.Font.Name = "Times New Roman";
             sheet.Cells.HorizontalAlignment = 3;
             sheet.Cells.VerticalAlignment = 2;
 
             //Первая строка
-            sheet.Cells[2, 1].Value = "Приложение №__ к Протоколу №________\n" +
+            sheet.Cells[firstRow + 1, 1].Value = "Приложение №__ к Протоколу №________\n" +
                                 "общего собрания собственников помещений\n" +
                                 "многоквартирного дома, расположенного, по адресу:\n" +
                                 "__________________________________________, от ______________";
-            sheet.Range[sheet.Cells[2, 1], sheet.Cells[2, 7]].Merge();
-            sheet.Cells[2, 1].HorizontalAlignment = 4;
+            sheet.Range[sheet.Cells[firstRow + 1, 1], sheet.Cells[firstRow + 1, 7]].Merge();
+            sheet.Cells[firstRow + 1, 1].HorizontalAlignment = 4;
 
             //Вторая строка
-            sheet.Cells[3, 1].Value = "Реестр собственников помещений многоквартирного дома по адресу:  "+ adress;
-            sheet.Cells[3, 1].Font.Bold = true;
-            sheet.Cells[3, 1].Font.Size = 10;
-            sheet.Range[sheet.Cells[3, 1], sheet.Cells[3, 7]].Merge();
+            sheet.Cells[firstRow + 2, 1].Value = "Реестр собственников помещений многоквартирного дома по адресу:  "+ adress;
+            sheet.Cells[firstRow + 2, 1].Font.Bold = true;
+            sheet.Cells[firstRow + 2, 1].Font.Size = 12;
+            sheet.Range[sheet.Cells[firstRow + 2, 1], sheet.Cells[firstRow+2, 7]].Merge();
 
             //Третья строка
-            sheet.Cells[4, 1].Value = "№п/п";
-            sheet.Cells[4, 2].Value = "№ пом.";
-            sheet.Cells[4, 3].Value = "Ф.И.О.";
-            sheet.Cells[4, 4].Value = "Документ, подтверждающий право собственности";
-            sheet.Cells[4, 5].Value = "Сведения о размере доли в праве общей собственности на общее имущество собственников помещений в МКД	";
-            sheet.Cells[4, 5].WrapText = true;
-            sheet.Cells[4, 7].Value = "Площадь";
-            sheet.Range[sheet.Cells[4, 1], sheet.Cells[5, 1]].Merge();
-            sheet.Range[sheet.Cells[4, 2], sheet.Cells[5, 2]].Merge();
-            sheet.Range[sheet.Cells[4, 3], sheet.Cells[5, 3]].Merge();
-            sheet.Range[sheet.Cells[4, 4], sheet.Cells[5, 4]].Merge();
-            sheet.Range[sheet.Cells[4, 5], sheet.Cells[4, 6]].Merge();
-            sheet.Range[sheet.Cells[4, 7], sheet.Cells[5, 7]].Merge();
+            sheet.Cells[firstRow + 3, 1].Value = "№п/п";
+            sheet.Cells[firstRow + 3, 2].Value = "№ пом.";
+            sheet.Cells[firstRow + 3, 3].Value = "Ф.И.О.";
+            sheet.Cells[firstRow + 3, 4].Value = "Документ, подтверждающий право собственности";
+            sheet.Cells[firstRow + 3, 5].Value = "Сведения о размере доли в праве общей собственности на общее имущество собственников помещений в МКД	";
+            sheet.Cells[firstRow + 3, 5].WrapText = true;
+            sheet.Cells[firstRow + 3, 7].Value = "Площадь";
+            sheet.Range[sheet.Cells[firstRow + 3, 1], sheet.Cells[firstRow + 4, 1]].Merge();
+            sheet.Range[sheet.Cells[firstRow + 3, 2], sheet.Cells[firstRow + 4, 2]].Merge();
+            sheet.Range[sheet.Cells[firstRow + 3, 3], sheet.Cells[firstRow + 4, 3]].Merge();
+            sheet.Range[sheet.Cells[firstRow + 3, 4], sheet.Cells[firstRow + 4, 4]].Merge();
+            sheet.Range[sheet.Cells[firstRow + 3, 5], sheet.Cells[firstRow + 3, 6]].Merge();
+            sheet.Range[sheet.Cells[firstRow + 3, 7], sheet.Cells[firstRow + 4, 7]].Merge();
 
             //Четвёртая строка
-            sheet.Cells[5, 5].Value = "размер доли ";
-            sheet.Cells[5, 6].Value = "кв.м от общей площади помещения";
-            sheet.Cells[5, 6].WrapText = true;
-
-            sheet.Range[sheet.Cells[6, 1], sheet.Cells[10000, 4]].HorizontalAlignment = 2;
-            sheet.Cells[2, 1].HorizontalAlignment = 4;
+            sheet.Cells[firstRow + 4, 5].Value = "размер доли ";
+            sheet.Cells[firstRow + 4, 6].Value = "кв.м от общей площади помещения";
+            sheet.Cells[firstRow + 4, 6].WrapText = true;
 
             //Конец NewFormXls
         }
 
         //Заполнение документа и сохранение в directory
-        static void MakeXls(List<Buildding> all_buildings, string directory)
+        static void MakeXls(List<Buildding> all_buildings, bool fillEmty, string directory)
         {
             Console.WriteLine("\nНачинаю работу с MS Excel. Придётся подождать. Но, если больше 5 минут, то я завис.\n");
             Application xlsApp = new Application();
@@ -238,15 +237,16 @@ namespace XmlToXls
                 sheet = (Worksheet)xlsApp.ActiveSheet;
 
                 //Ставим значения
-                int roll = 6, n = 1;
+                int roll = 5, n = 1;
                 foreach (Item_flat flat in buildding.flats)
                 {
-                    Console.WriteLine("Пишем: " + flat.address + " кв. " + flat.numFlat);
+                    Console.WriteLine("Пишем: " + flat.address + ", " + flat.numFlat);
                     if (flat.owners.Count == 0)
                         Console.WriteLine("     Не нашёл информации о владельцах.");
 
-                    foreach (var owner in flat.owners)
+                    foreach (Owner owner in flat.owners)
                     {
+                        sheet.Range[sheet.Cells[roll, 1], sheet.Cells[roll, 4]].HorizontalAlignment = 2;
                         sheet.Cells[roll, 1].Value = n;
                         sheet.Cells[roll, 2].Value = flat.numFlat;
                         sheet.Cells[roll, 3].Value = owner.fio;
@@ -257,6 +257,20 @@ namespace XmlToXls
                         n++;
                         roll++;
                     }
+                    //Если не найдены собственники
+                    if(fillEmty && flat.owners.Count() == 0)
+                    {
+                        sheet.Range[sheet.Cells[roll, 1], sheet.Cells[roll, 4]].HorizontalAlignment = 2;
+                        sheet.Cells[roll, 1].Value = n;
+                        sheet.Cells[roll, 2].Value = flat.numFlat;
+                        sheet.Cells[roll, 3].Value = " - нет данных - ";
+                        sheet.Cells[roll, 4].Value = " --- ";
+                        sheet.Cells[roll, 5].Value = " - ";
+                        sheet.Cells[roll, 6].Value = " - ";
+                        sheet.Cells[roll, 7].Value = flat.area;
+                        n++;
+                        roll++;
+                    }                    
                 }
                 //Сохраняем и закрываем книгу
                 try
@@ -272,7 +286,8 @@ namespace XmlToXls
                 }
                 //book.SaveAs(directory + buildding.address
                 try { book.SaveAs(directory + buildding.address); }
-                catch { Console.WriteLine("Ошибка сохранения."); Console.ReadKey(); }
+                catch { Console.WriteLine("Ошибка сохранения xlsx. Возможно файл открыт в другой программе" +
+                    "\nЧтобы продолжить можно нажать Enter.");Console.ReadKey(); }
                 book.Close(true);
             }
             xlsApp.Quit();
@@ -326,10 +341,40 @@ namespace XmlToXls
                                         address += node_adr.Attribute("Value").Value;
                                         break;
                                     case ("Apartment"): //номер квартиры
-                                        number_flat_string = node_adr.Attribute("Value").Value;
+                                        number_flat_string = node_adr.Attribute("Value").Value + " кв.";
                                         try {number_flat = Convert.ToInt32(node_adr.Attribute("Value").Value); }
-                                        catch { number_flat = Int32.MaxValue; Console.Write(" Плохой номер помещения. "); }
-                                        
+                                        catch { number_flat = Int32.MaxValue;/* Console.WriteLine(" Плохой номер помещения. "); */}
+                                        break;
+                                    case ("Other"): //что-то другое
+
+                                        string type = "", analise = ""; //Временные строки
+                                        if (node_adr.Value.Length > 12 && node_adr.Value.Length <30 && node_adr.Value.Substring(0, 12) == "машино-место")
+                                            { type = "м/м"; analise = node_adr.Value.Substring(12); }
+                                        else if (node_adr.Value.Substring(node_adr.Value.Length - 3) == "м/м")
+                                        { type = "м/м";
+                                            if (node_adr.Value.Length < 12) analise = node_adr.Value.Substring(0, node_adr.Value.Length - 3);
+                                            else
+                                            {analise = node_adr.Value.Substring(node_adr.Value.Length - 22);}
+                                        }
+
+                                        if (type == "м/м")
+                                        {
+                                            //Выдераем "a/b"
+                                            bool notOne = false;
+                                            string number = "";
+                                            int num = 0, den = 0;
+                                            foreach (char ch in analise)
+                                                if (Char.IsNumber(ch))
+                                                    number += ch;
+                                                else if (num != 0) { den = Convert.ToInt32(number); break; }
+                                                else if (ch == ',' && number != "") { number_flat_string += number + ","; number = ""; notOne = true; }
+                                                else if (ch == '/' && number != "") { num = Convert.ToInt32(number); number = ""; }
+
+                                            //Вносим в ячейку
+                                            number_flat = 5000 + den * 400 + num;
+                                            if (notOne) number_flat = 5000 + den * 400; //Переделываем
+                                            number_flat_string += num + "/" + den + " " + type;
+                                        }
                                         break;
 
                                     default:
@@ -469,9 +514,50 @@ namespace XmlToXls
                 {
                     try { Flats.Add(XmlProcessing(XDocument.Load(xml_filename))); }
                     catch { continue; };
-                    Console.WriteLine("Прочитал: " + Flats.Last().address + " кв. " + Flats.Last().numFlat);
+                    Console.WriteLine("Прочитал: " + Flats.Last().address + ", " + Flats.Last().numFlat);
                 }
             }
+        }
+        
+        //Переименование Xml файлов 
+        static void RenameXML(IEnumerable<string> FileNames)
+        {
+            Console.WriteLine("\nКопирую с переименованием найденные XML в папку \"Переименованные файлы\".");
+            //Временная директориz и папка для сохранения
+            string home = Directory.GetCurrentDirectory() + "\\";
+            string target = home + "Переименованные файлы\\";
+
+            try { Directory.CreateDirectory(target); }
+            catch { Console.WriteLine("Ошибка при создании папки."); }
+
+            if (FileNames.Count() > 0)
+                foreach (string xml_filename in FileNames)
+                {
+                    //Получение данных об объекте
+                    Item_flat flat = XmlProcessing(XDocument.Load(xml_filename));
+
+                    //Устранение непригодных символов
+                    string newAdr = "";
+                    foreach (char ch in target + flat.address + ", " + flat.numFlat + ".xml")
+                    {
+                        if (ch == '/')
+                            newAdr += '-';
+                        else
+                            newAdr += ch;
+                    }
+                    //Проверка на наличие такого файла и удаление
+                    try {if (File.Exists(newAdr)) File.Delete(newAdr);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Ошибка при копировании XML." +
+                    "Не удалось удалить старый файл, возможно он открыт в каком-то приложении.");
+                    }
+                        //копирование
+                     try { File.Copy(xml_filename, newAdr); }
+                     catch { Console.WriteLine("Ошибка при копировании XML."); continue; }                        
+                    
+                }     
         }
 
         //Извлечение XML из архивов
@@ -505,7 +591,7 @@ namespace XmlToXls
                         {
                             //Выгрузка
                             Item_flat flat = XmlProcessing(XDocument.Load(xml_filename));
-                            File.Copy(xml_filename, target + flat.address + " кв " + flat.numFlat + ".xml");
+                            File.Copy(xml_filename, target + flat.address + ", " + flat.numFlat + ".xml");
                         }
                     }
                 }
@@ -543,7 +629,7 @@ namespace XmlToXls
                         foreach (string xml_filename in Directory.EnumerateFiles(temp, "*.xml", SearchOption.AllDirectories))
                         {
                             Flats.Add(XmlProcessing(XDocument.Load(xml_filename)));
-                            Console.WriteLine("Прочитал: " + Flats.Last().address + " кв. " + Flats.Last().numFlat);
+                            Console.WriteLine("Прочитал: " + Flats.Last().address + ", " + Flats.Last().numFlat);
 
                             //Выгрузка
                             //Directory.CreateDirectory(home + "\\Выгрузка\\");
@@ -608,18 +694,17 @@ namespace XmlToXls
             //Задаём Стартовую директорию
             string home = Directory.GetCurrentDirectory() + "\\";
 
-            //Все квартиры из всех файлов
-            List<Item_flat> All_flats = new List<Item_flat>();
-
             // --- Работа с XML
             //Создаём коллекцию нужных файлов в дериктории
-            IEnumerable<string> All_XML = Directory.EnumerateFiles(home, "*.xml", SearchOption.AllDirectories);
-            IEnumerable<string> All_Zip = Directory.EnumerateFiles(home, "*.zip", SearchOption.AllDirectories);
+            IEnumerable<string> All_XML = Directory.EnumerateFiles(home, "*.xml", SearchOption.AllDirectories).ToList();
+            IEnumerable<string> All_Zip = Directory.EnumerateFiles(home, "*.zip", SearchOption.AllDirectories).ToList();
             //Выводим общие число файлов
             Console.WriteLine($"Найдено {All_Zip.Count()} каких-то архивов и {All_XML.Count()} каких-то XML.");
             Console.Write("\nВыберите необходимый алгоритм действий:\n\n" +
-                "1 - Обработать все архивы и XML во всех вложенных папках рядом с этой програмкой, получить реестры каждого упомянутого в них здания\n" +
+                "1 - Обработать все архивы и XML во всех вложенных папках рядом с этой програмкой, \n" +
+                "      получить реестры каждого упомянутого в них здания\n" +
                 "2 - Извлечь XML из архивов в новую папку\n" +
+                "3 - Собрать все XML во вложенных папкав в одну и назвать по адресу объекта\n" +
                 "Ввидите номер выбранного варианта и нажмите Enter: ");
             int choice = 0;
             try { choice = Convert.ToInt32(Console.ReadLine()); }
@@ -628,17 +713,42 @@ namespace XmlToXls
 
             switch (choice)
             {
-                case 1:
+                case 1: //Обработка всех файлов
                     {
+                        //Выбор отображения
+                        bool choiceVisible = false;
+                        Console.Write("\n\nЗаписывать объекты для которых не удалось найти собственников?\n" +
+                           "1 - Да. Отоброжать.\n" +
+                           "2 - Нет. Не отображать. (по умолчанию)\n" +
+                           "Ввидите номер выбранного варианта и нажмите Enter: ");
+                        try { if (Convert.ToInt32(Console.ReadLine()) == 1) choiceVisible = true; }
+                        catch { Console.WriteLine("Некорретное значение."); }
+
+                        //Все квартиры из всех файлов
+                        List<Item_flat> All_flats = new List<Item_flat>();
+
                         //Чтение всехх Xml файлов 
                         ReedXML(ref All_flats, All_XML);
                         //Проход по архивам (если есть)
                         ReedZip(ref All_flats, All_Zip);
+
+                        // Получим список строений
+                        List<Buildding> All_buildings = MakeBuildings(All_flats);
+
+                        // Создаём Файлы реестров
+                        MakeXls(All_buildings, choiceVisible, home);
                         break;
                     }
-                case 2:
+                case 2: //Распаковка архивов
                     {
                         UnZip(All_Zip);
+                        Console.WriteLine("\nЧтобы закрыть можно нажать Enter.");
+                        Console.Read();
+                        return;
+                    }
+                case 3: //Переименование файлов
+                    {
+                        RenameXML(All_XML);
                         Console.WriteLine("\nЧтобы закрыть можно нажать Enter.");
                         Console.Read();
                         return;
@@ -651,12 +761,6 @@ namespace XmlToXls
                         return;
                     }
             }
-
-            // Получим список строений
-            List<Buildding> All_buildings = MakeBuildings(All_flats);
-
-            // Создаём Файлы реестров
-            MakeXls(All_buildings, home);
 
             Console.WriteLine("\nЧтобы закрыть можно нажать Enter.");
             Console.Read();
